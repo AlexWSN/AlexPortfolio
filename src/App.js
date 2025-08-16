188dea37c91cdbae368479e57787f5e16f24d563
AlexWSN <sasualexandra97@gmail.com>
2025-08-16 12:51:52 +0300

    background

--- src/App.js
+++ src/App.js
@@ -7,17 +7,33 @@ import Certificate from "./components/Certificate";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 
+
 function App() {
+  const backgroundStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/background.jpg)`,
+    backgroundAttachment: "fixed",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+    position: "relative",
+    overflow: "hidden",
+  };
+
   return (
-    <div>
+    <>
       <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Certificate />
-      <Experience />
-      <Contact />
-    </div>
+      <div style={backgroundStyle}>
+        <div className="pt-20 relative">
+          <Hero />
+          <About />
+          <Projects />
+          <Certificate />
+          <Experience />
+          <Contact />
+        </div>
+      </div>
+    </>
   );
 }
 
